51615835ed11c5d8f493ee1d7e31b3dedb058442
Bodhish Thomas <bodhish@gmail.com>
2020-04-13 01:52:22 +0530

    Update user source

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,7 +1,7 @@
 class Consultation < ApplicationRecord
   enum consultation_type: { doctor: "doctor", pharmacy: "pharmacy", nursing_need: "nursing_need", palliative_care: "palliative_care", others: "others" }
   enum status: { pending: "pending", called: "called", not_reachable: "not_reachable" }
-  enum source: { deoc: "deoc", police: "police", private_hospital: "private_hospital", migrant_project: "migrant_project", medical_practitioner: "medical_practitioner" }
+  enum source: { ima: 'ima', deoc: "deoc", police: "police", private_hospital: "private_hospital", migrant_project: "migrant_project", medical_practitioner: "medical_practitioner" }
   enum current_status: { high_risk: "high_risk", health_care_personnel: "health_care_personnel", general_public: "general_public" }
   enum category: { a: "a", b1: "b1", b2: "b2", c: "c", uncategorized: "uncategorized" }
   enum test_status: { not_tested: "not_tested", positive: "positive", negative: "negative", awaiting: "awaiting", test_invalid: "test_invalid" }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable
 
   enum role: { phone_caller: "phone_caller", district_admin: "district_admin", doctor: "doctor", admin: "admin" }
-  enum source: { ima: 'IMA', deoc: "deoc", police: "police", private_hospital: "private_hospital", migrant_project: "migrant_project", medical_practitioner: "medical_practitioner" }
+  enum source: { ima: 'ima', deoc: "deoc", police: "police", private_hospital: "private_hospital", migrant_project: "migrant_project", medical_practitioner: "medical_practitioner" }
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
