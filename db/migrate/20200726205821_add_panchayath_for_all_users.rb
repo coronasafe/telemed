1992317e100f08321f35e3bb170479961a40f621
Bodhish Thomas <bodhish@gmail.com>
2020-07-27 02:34:16 +0530

    seed panchayath for all users

---
+++ db/migrate/20200726205821_add_panchayath_for_all_users.rb
@@ -0,0 +1,11 @@
+class AddPanchayathForAllUsers < ActiveRecord::Migration[6.0]
+  def change
+    p = Panchayat.find_by(name: "Ernakulam  District Panchayat")&.id.presence || 1
+    User.all.each do |u|
+      next if u.panchayat_id.present?
+
+      u.panchayat_id = p
+      c.save(touch: false)
+    end
+  end
+end
